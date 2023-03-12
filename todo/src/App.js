be14f7bd25012f536e9ec0016ede35a84c357338
Palldanerar <103989677+Palldanerar@users.noreply.github.com>
2023-03-12 21:08:18 +0300

    Update task

--- todo/src/App.js
+++ todo/src/App.js
@@ -1,10 +1,27 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import Task from './Task';
 
 function App() {
+
+  const [tasks, setTasks] = React.useState([])
+  const [task, setTask] = React.useState("")
+
+  const newTask = () => {
+    setTasks([...tasks, {title: task}])
+    setTask("")
+  }
+
   return (
     <div className="App">
-      
+      <input type="text" value={task} onChange={e => setTask(e.target.value)} />
+      <button onClick={newTask}>Новое Задание</button>
+
+      {
+        tasks.map(task => {
+          return <Task tasks={task} />
+        })
+      }
     </div>
   );
 }
---
+++ todo/src/Task.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Task(props) {
+  return (
+    <div>
+      <h1>{props.tasks.title}</h1> 
+    </div>
+  )
+}
+
+export default Task
