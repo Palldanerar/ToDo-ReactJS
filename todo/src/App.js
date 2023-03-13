fb7d38f97c2cad908bf8ab422beefc361b6fbe8d
Palldanerar <103989677+Palldanerar@users.noreply.github.com>
2023-03-13 20:11:58 +0300

    Remove

--- todo/src/App.js
+++ todo/src/App.js
@@ -1,27 +1,38 @@
-import React from 'react';
-import './App.css';
-import Task from './components/Task/Task';
-import Header from './components/Header/Header';
+import React from "react";
+import "./App.css";
+import Task from "./components/Task/Task";
+import Header from "./components/Header/Header";
 
 function App() {
-
-  const [tasks, setTasks] = React.useState([])
-  const [task, setTask] = React.useState("")
+  const [tasks, setTasks] = React.useState([]);
+  const [task, setTask] = React.useState("");
 
   const newTask = () => {
-    if (!task) return
-    setTasks(prev => [...tasks, {id: `${prev.length + 1}`, title: task}])
-    setTask("")
-  }
+    if (!task) return;
+    setTasks((prev) => [...tasks, { id: `${prev.length + 1}`, title: task }]);
+    setTask("");
+  };
+
+  const removeTask = (index) => {
+    setTasks(tasks.filter((task, id) => id !== index));
+  };
 
   return (
     <div className="App">
-      <Header task={task} setTask={setTask} newTask={newTask} />
-      {
-        tasks.map(task => {
-          return <Task key={task.id} tasks={task}/>
-        })
-      }
+      <Header
+        task={task}
+        setTask={setTask}
+        newTask={newTask}
+      />
+      {tasks.map((task, index) => {
+        return (
+          <Task
+            removeTask={() => removeTask(index)}
+            key={task.id}
+            tasks={task}
+          />
+        );
+      })}
     </div>
   );
 }
--- todo/src/components/Task/Task.js
+++ todo/src/components/Task/Task.js
@@ -1,10 +1,13 @@
 import React from 'react'
 import './Task.css'
 
-function Task({ tasks }) {
+function Task({ tasks, removeTask }) {
   return (
     <div className='task'>
+      <div className='in-task'>
       <h1>{tasks.title}</h1>
+      <button className='btn-remove' onClick={removeTask}>Удалить</button>
+      </div>
       <hr />
     </div>
   )
